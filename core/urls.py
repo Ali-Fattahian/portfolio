c879d7f10af65b45043bee97e81d4ba79f9c7ada
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-08-01 14:06:12 +0330

    make create-message end points

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+from .views import create_message
 
 
 urlpatterns = [
-    # path('create-message', )
+    path('create-message', create_message),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Message
+from threading import Thread
 
-# Create your views here.
+
+def new_message(author, email, subject, content):
+    Message.objects.create(author=author, email=email,
+                           subject=subject, content=content)
+
+
+def create_message(request):
+    if request.method == 'POST':
+        author = request.POST.get('author')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        content = request.POST.get('message')
+
+        if author and email and subject and content:
+            thread = Thread(target=new_message)
+            thread.start()
+            return HttpResponse('Thanks for sending the message,\
+                                 i will try to send you a reponse\
+                                 email as soon as i can.', status=201)
+        return HttpResponse('Please fill out all the fields.',
+                             status=400)
+    
+    return HttpResponse('You can only send a post request\
+                         to this route', status=405)
