dff454ef8dca7f2789103a6c02e3e87ee4c60fed
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-08-01 13:24:39 +0330

    create Message model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    author = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    content = models.TextField()
+
+    def __str__(self):
+        return f'{self.email}: {self.subject}'
